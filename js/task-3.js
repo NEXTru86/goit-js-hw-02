3e3d12a2dedc1f4cf51e6004f18ef0807d27e673
NEXTru86 <kharinpav86@gmail.com>
2020-12-20 23:58:05 +0200

    Выполнил задачи 2 - 4

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+const findLongestWord = function(string) {
+    const arr = string.split(' ');
+    let maxWord = arr[0];
+    
+    for (const word of arr) {
+        word.length > maxWord.length ? maxWord = word : 0;
+    }
+    return maxWord;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
